5cfd372fbae59d594389c09dc665a0d76d4d242c
DoaaSalmen <doaasalmen258@gmail.com>
2025-11-13 21:06:27 +0100

    Morgan merged with Winston

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
+const morgan = require('morgan'); 
 const winston = require('winston');
 const app = express();
 
-app.use(helmet());
+dotenv.config(); 
 
+app.use(helmet());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -28,22 +30,42 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({ filename: 'logs/app.log' }) 
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'logs/exceptions.log' }),
+    new winston.transports.Console()
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ filename: 'logs/rejections.log' }), 
+    new winston.transports.Console()
   ]
 });
 
-//home page
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => logger.info(message.trim()), 
+    },
+  })
+);
+
 app.get('/', (req, res) => {
-  logger.info('Home page');
+  logger.info('Home page accessed');
   res.send('Welcome!');
 });
-//error
+
 app.get('/error', (req, res) => {
   try {
     throw new Error('Test error');
   } catch (err) {
-    logger.error(`error: ${err.message}`);
+    logger.error(`Error: ${err.message}`);
     res.status(500).send('Server error');
   }
+});
+
+app.get('/promise-error', (req, res) => {
+  Promise.reject(new Error('Unhandled promise rejection!'));
+  res.send('This will trigger an unhandled rejection.');
 });
 
 const PORT = process.env.PORT || 3000;
